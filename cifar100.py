369c59eed17f036a54be44b6504f088cf9a29a17
JiHyunKim <80038485+zhixxian@users.noreply.github.com>
2022-04-12 20:58:46 +0900

    Update cifar100.py

--- cifar100.py
+++ cifar100.py
@@ -8,11 +8,13 @@ import torchvision
 from torch.utils.data import DataLoader,Dataset
 
 import cv2
+import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import PIL.Image
+from PIL import Image
 
+import torch.multiprocessing
 import torch
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
@@ -22,53 +24,76 @@ writer = SummaryWriter()
 batch_size = 64
 
 class Cifar100CustomDataset(Dataset):
-    def __init__(self, img_path, annotation, transform):
+    def __init__(self, img_path, annotation, transform=None):
+        # self.to_tensor = torchvision.transforms.ToTensor()
+        self.transform = transform
         self.img_path = img_path
         self.annotation = pd.read_csv(annotation)
-        # self.transform = transform
         # self.transform = transforms.Compose([transforms.ToTensor()])
-        self.transform = torchvision.transforms.ToTensor()
+        #self.trans = torchvision.transforms.ToPILImage()      
+        self.transform = transform
+
+        self.image_list = glob.glob(self.img_path + '/*.jpg')  
         
-        self.img_names = self.annotation[:]['image_id']
-        self.labels = self.annotation[:]['fine_label_names']
-        
+        self.Image_list = []
+        for self.img_path in self.image_list:
+            self.Image_list.append(Image.open(self.img_path))        
+            
+        # self.img_names = self.annotation[:]['image_id'] # 이미지 이름
+        self.label_names = self.annotation[:]['fine_label_names'] # 라벨 이름
+        self.label_list = [0] * len(self.label_names)
     def __len__(self):
-        return len(self.img_names)
+        return len(self.image_list) # 이미지개수
     
     def __getitem__(self, idx):
-        # image = self.img_path + '/*.png'
-        image = cv2.imread(self.img_path + self.img_names.iloc[idx])
-        image = self.transform(image)
-        labels = self.labels[idx]
-        sample = {'image' : image, 'labels' : labels}
+        self.img_path = self.Image_list[idx]
+        label = self.label_list[idx]
+        img = Image.open(self.img_path)
         
-        return sample
+        if self.transform is not None:
+            img = self.transform(img)
+        # image = cv2.imread(self.img_names.iloc[idx])
+        # image = cv2.imread(self.img_path)
+        # image_1 = self.transform(image)
+        # if self.transform is not None:
+        #     image = self.transform(image)
+        # labels = np.array([self.labels])
+
+        # labels = self.labels[idx]
+        # sample = {'image' : image, 'label' : labels}
+        
+        return img, label
+
+
      
 train_transform = transforms.Compose([
                 transforms.ToPILImage(),
-                transforms.Resize((400, 400)),
+                # transforms.Resize((400, 400)),
                 transforms.ToTensor()])
 
 test_transform =transforms.Compose([
                 transforms.ToPILImage(),
-                transforms.Resize((400, 400)),
+                # transforms.Resize((400, 400)),
                 transforms.ToTensor()])
 
-train_dataset=Cifar100CustomDataset(img_path='/HDD/zhixian/Cifar100/train_images',
+train_dataset=Cifar100CustomDataset(img_path='/HDD/zhixian/Cifar100/train_images/',
                                     annotation='/HDD/zhixian/Cifar100/train.csv',
                                     transform=train_transform)
-test_dataset=Cifar100CustomDataset(img_path='/HDD/zhixian/Cifar100/test_images',
+test_dataset=Cifar100CustomDataset(img_path='/HDD/zhixian/Cifar100/test_images/',
                                     annotation='/HDD/zhixian/Cifar100/test.csv',
                                     transform=test_transform)
 
 train_dataloader = DataLoader(
     train_dataset, 
     batch_size=batch_size,
-    shuffle=True
+    # transform=train_transform,
+    shuffle=False,
+    num_workers=4
 )
 
 test_dataloader = DataLoader(
     test_dataset, 
-    batch_size=4,
-    shuffle=True
+    batch_size=batch_size,
+    # transform=test_transform,
+    shuffle=False
 )
