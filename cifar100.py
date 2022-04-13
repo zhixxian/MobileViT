bda541a4adac5fd8444a83b117df76eb9052d2e1
JiHyunKim <80038485+zhixxian@users.noreply.github.com>
2022-04-13 15:54:03 +0900

    Update cifar100.py

--- cifar100.py
+++ cifar100.py
@@ -25,54 +25,35 @@ writer = SummaryWriter()
 import torch.multiprocessing
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-# img_path = '/HDD/zhixian/Cifar100/'
-# annotation_path = '/HDD/zhixian/Cifar100/train.csv'
 batch_size = 64
 
 class Cifar100CustomDataset(Dataset):
     def __init__(self, img_path, annotation, transform=None):
-        # self.to_tensor = torchvision.transforms.ToTensor()
+        # self.img_path = 이미지 폴더 경로
+        # self.anntation = 이미지 레이블 
+      
         self.transform = transform
         self.img_path = img_path
         self.annotation = pd.read_csv(annotation)
-        # self.transform = transforms.Compose([transforms.ToTensor()])
-        #self.trans = torchvision.transforms.ToPILImage()      
         self.transform = transform
 
-        self.image_list = glob.glob(self.img_path + '/*.jpg')  
-        
-        # self.Image_list = []
-        # for self.img_path in self.image_list:
-        #     self.Image_list.append(Image.open(self.img_path))        
-            
-        # self.img_names = self.annotation[:]['image_id'] # 이미지 이름
+        self.image_list = glob.glob(self.img_path + '/*.jpg') # 이미지 가져오기
+       
+        self.img_names = self.annotation[:]['image_id'] # 이미지 이름
         self.label_names = self.annotation[:]['fine_label_names'] # 라벨 이름
-        self.label_list = [0] * len(self.label_names)
+        self.label_list = [0] * len(self.label_names) # # 라벨 이름 리스트화
+    
     def __len__(self):
-        return len(self.image_list) # 이미지개수
+        return len(self.image_list) # 이미지개수 리턴
     
     def __getitem__(self, idx):
-        # self.img_path = self.Image_list[idx]
         self.img_path = self.image_list[idx]
         label = self.label_list[idx]
-        # img = Image.open(self.img_path)
-        img = ToTensor()(self.img_path).unsqueeze(0)
+        img = ToTensor()(self.img_path).unsqueeze(0) # 여기서부터 55번째 줄 transform tensor 관련 오류로 인해 수정해본 부분입니다.
         img = torch.Tensor(img)
         img = Variable(img)
-        img = transforms.ToPILImage()(img)
-        
-        # if self.transform is not None:
-        #     img = self.transform(img)
-        # image = cv2.imread(self.img_names.iloc[idx])
-        # image = cv2.imread(self.img_path)
-        # img = self.transform(img)
-        if self.transform is not None:
-            image = self.transform(image)
-        # labels = np.array([self.labels])
-
-        # labels = self.labels[idx]
-        # sample = {'image' : image, 'label' : labels}
-        
+        img = transforms.ToPILImage()(img) # 여기까지!
+            
         return img, label
 
 
@@ -97,14 +78,11 @@ test_dataset=Cifar100CustomDataset(img_path='/HDD/zhixian/Cifar100/test_images/'
 train_dataloader = DataLoader(
     train_dataset, 
     batch_size=batch_size,
-    # transform=train_transform,
     shuffle=False,
-    # num_workers=4
 )
 
 test_dataloader = DataLoader(
     test_dataset, 
     batch_size=batch_size,
-    # transform=test_transform,
     shuffle=False
 )
