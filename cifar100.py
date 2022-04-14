2ed599045a986737948d9403adea9e9361b4249a
JiHyunKim <80038485+zhixxian@users.noreply.github.com>
2022-04-14 13:51:38 +0900

    Update cifar100.py

--- cifar100.py
+++ cifar100.py
@@ -1,11 +1,6 @@
 # from operator import index
-from __future__ import annotations
-from sklearn.semi_supervised import LabelSpreading
-import torch
-import torchvision.transforms as transforms
-import torchvision
 
-from torch.utils.data import DataLoader,Dataset
+from skimage import io
 
 import cv2
 import glob
@@ -14,48 +9,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-import torch.multiprocessing
 import torch
+import torchvision
+import torchvision.transforms as transforms
 from torchvision.transforms import ToTensor
 from torch.autograd import Variable
-
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
-
-import torch.multiprocessing
-torch.multiprocessing.set_sharing_strategy('file_system')
+from torch.utils.data import DataLoader,Dataset
 
 batch_size = 64
 
-class Cifar100CustomDataset(Dataset):
-    def __init__(self, img_path, annotation, transform=None):
-        # self.img_path = 이미지 폴더 경로
-        # self.anntation = 이미지 레이블 
-      
-        self.transform = transform
+class Cifar100CustomDataset(Dataset.ImageFolder):
+    def __init__(self, img_path, annotation, transform = None):
+        '''
+        self.img_path = 이미지 폴더 경로
+        self.annotation = 라벨 csv 파일 경로
+        '''
         self.img_path = img_path
         self.annotation = pd.read_csv(annotation)
         self.transform = transform
-
-        self.image_list = glob.glob(self.img_path + '/*.jpg') # 이미지 가져오기
-       
-        self.img_names = self.annotation[:]['image_id'] # 이미지 이름
-        self.label_names = self.annotation[:]['fine_label_names'] # 라벨 이름
-        self.label_list = [0] * len(self.label_names) # # 라벨 이름 리스트화
-    
+        
     def __len__(self):
-        return len(self.image_list) # 이미지개수 리턴
+        return len(self.annotation)
     
-    def __getitem__(self, idx):
-        self.img_path = self.image_list[idx]
-        label = self.label_list[idx]
-        img = ToTensor()(self.img_path).unsqueeze(0) # 여기서부터 55번째 줄 transform tensor 관련 오류로 인해 수정해본 부분입니다.
-        img = torch.Tensor(img)
-        img = Variable(img)
-        img = transforms.ToPILImage()(img) # 여기까지!
+    def __getitem__(self,idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
             
-        return img, label
-
+        img_name = os.path.join(self.img_path, str(self.annotation.iloc[:]['image_id'])) # 이미지를 불러오기 위해 이미지 path와 jpg 이름 합쳐줌
+        image = io.imread(img_name)
+        label = self.annotation.iloc[:]['fine_label_names'] # train.csv에서 라벨 부분만 갖고옴
+        label = np.array([label]) # 라벨 배열로 갖고오기
+        
+        sample = {'image':image, 'label':label}
+        
+        if self.transform:
+            sample = self.transform(sample)
+            
+        return sample
 
 train_transform = transforms.Compose([
                 transforms.ToTensor(),
