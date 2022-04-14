cbcbbf4ae7abd7d71ad9ed3dbd07923cf0aa44ae
JiHyunKim <80038485+zhixxian@users.noreply.github.com>
2022-04-14 21:46:38 +0900

    Update cifar100.py

--- cifar100.py
+++ cifar100.py
@@ -20,7 +20,8 @@ import os
 batch_size = 64
 
 class Cifar100CustomDataset(Dataset):
-    def init(self, img_path, annotation, transform = None):
+    def __init__(self, img_path, annotation, transform = None):
+        super().__init__()
         '''
         self.img_path = 이미지 폴더 경로
         self.annotation = 라벨 csv 파일 경로
@@ -29,30 +30,39 @@ class Cifar100CustomDataset(Dataset):
         self.annotation = pd.read_csv(annotation)
         self.transform = transform
         
-    def len(self):
+    def __len__(self):
         return len(self.annotation)
     
-    def getitem(self,idx):
+    def __getitem__(self,idx):
             
         img_name = os.path.join(self.img_path, str(self.annotation.iloc[idx]['image_id'])) # 이미지를 불러오기 위해 이미지 path와 jpg 이름 합쳐줌
         image = io.imread(img_name)
-        label = self.annotation.iloc[:]['fine_label_names'] # train.csv에서 라벨 부분만 갖고옴
-        label = np.array([label]) # 라벨 배열로 갖고오기
-
-        sample = {'image':image, 'label':label}
+        label = self.annotation.iloc[idx]['fine_label_names'] # train.csv에서 라벨 부분만 갖고옴
+        # label = np.asarray([label]) # 라벨 배열로 갖고오기
+        # sample = {'image':image, 'label':label}
         
-        if self.transform:
-            sample['image'] = self.transform(sample['image'])
+        # image = self.transform(sample['image'])
+        # label = sample['label']
+        labels_unique = set(label)
+        keys = {key: value for key, value in zip(labels_unique, range(len(labels_unique)))}
+        labels_onehot = torch.zeros(size=(len(label), len(keys)))
+        for idx, label in enumerate(labels_onehot):
+            labels_onehot[idx][keys[label]] = 1
+        
+        # if self.transform:
+        #     image = self.transform(image)
+            
+        # return image, label
+        # return self.transform(image), self.transform(label)
          
-        return sample['image'], sample['label']
+        return image, labels_onehot
+        # return sample
 
 train_transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Resize((400, 400))])
+                transforms.ToTensor()])
 
 test_transform =transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Resize((400, 400))])
+                transforms.ToTensor()])
 
 
 train_dataset=Cifar100CustomDataset(img_path='/HDD/zhixian/Cifar100/train_images/',
